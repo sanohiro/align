fbc9bddc1222f470775880c4b827526e1618b2a6
Hironobu Sano <sano_h@icloud.com>
2026-07-17 19:16:54 +0900

    feat: update editor plugins for v0.1.0 specification

--- editors/emacs/align-mode.el
+++ editors/emacs/align-mode.el
@@ -13,52 +13,71 @@
     map))
 
 (defconst align-keywords
-  '("fn" "return" "mut" "pub" "module" "import" "if" "else" "arena" "box" "task_group" "match" "template" "unsafe" "extern" "as" "out"))
+  '("fn" "return" "mut" "pub" "module" "import" "if" "else" "arena" "task_group" "match" "loop" "break" "template" "unsafe" "extern" "as" "out"))
+
+(defconst align-attributes
+  '("align" "layout" "link"))
 
 (defconst align-types
-  '("i8" "u8" "i16" "u16" "i32" "u32" "i64" "u64" "f32" "f64" "bool" "char" "str" "string" "array" "slice" "raw" "writer" "reader" "buffer" "rng" "tcp_conn" "tcp_listener" "udp_socket" "child" "soa" "Option" "Result" "vec2" "vec4" "vec8" "vec16" "mask2" "mask4" "mask8" "mask16"))
+  '("i8" "u8" "i16" "u16" "i32" "u32" "i64" "u64" "f32" "f64" "bool" "char" "str" "string" "array" "slice" "soa" "box" "raw" "builder" "writer" "reader" "buffer" "array_builder" "file" "rng" "tcp_conn" "tcp_listener" "udp_socket" "child" "Option" "Result" "Error" "Num" "Ord" "Eq" "vec2" "vec4" "vec8" "vec16" "mask2" "mask4" "mask8" "mask16"))
 
 (defconst align-constants
   '("true" "false"))
 
 (defconst align-builtins
-  '("map" "par_map" "where" "reduce" "scan" "partition" "group_by" "sort" "sort_by_key" "chunks" "sum" "min" "max" "count" "any" "all" "dot"))
+  '("map" "par_map" "where" "reduce" "scan" "partition" "group_by" "agg" "sort" "sort_by_key" "chunks" "zip" "to_array" "map_into" "to_soa" "dict_encode" "sum" "min" "max" "count" "any" "all" "dot" "sum_where" "select" "fma" "abs" "sqrt" "floor" "ceil" "round" "trunc" "pow" "Some" "None" "Ok" "Err" "error" "map_err" "spawn" "wait" "print"))
+
+(defconst align-number-regexp
+  "\\_<\\(?:0[xX][[:xdigit:]_]+\\|0[oO][0-7_]+\\|0[bB][01_]+\\|[0-9][0-9_]*\\(?:\\.[0-9][0-9_]*\\)?\\(?:[eE][+-]?[0-9][0-9_]*\\)?\\)\\_>")
+
+(defconst align-operators
+  '(".." ":=" "->" "=>" "==" "!=" "<=" ">=" "&&" "||" "<<" ">>"
+    "=" "+" "-" "*" "/" "%" "<" ">" "!" "&" "|" "^" "~" "?"))
+
+(defconst align-operator-regexp
+  (regexp-opt align-operators))
+
+(defconst align-attribute-regexp
+  (concat "\\_<" (regexp-opt align-attributes) "\\_>\\(?=\\s-*(\\)"))
 
 (defconst align-font-lock-keywords
   `((,(regexp-opt align-keywords 'words) . font-lock-keyword-face)
+    (,align-attribute-regexp . font-lock-preprocessor-face)
     (,(regexp-opt align-types 'words) . font-lock-type-face)
     (,(regexp-opt align-builtins 'words) . font-lock-builtin-face)
-    (,(regexp-opt align-constants 'words) . font-lock-constant-face)))
+    (,(regexp-opt align-constants 'words) . font-lock-constant-face)
+    (,align-number-regexp . font-lock-constant-face)
+    (,align-operator-regexp . font-lock-keyword-face)))
+
+(defun align--command (subcommand)
+  "Build the command prefix for an Align SUBCOMMAND."
+  (let ((proj-root (locate-dominating-file default-directory "Cargo.toml")))
+    (if proj-root
+        (format "cargo run -q --manifest-path %s --bin alignc -- %s "
+                (shell-quote-argument (expand-file-name "Cargo.toml" proj-root))
+                subcommand)
+      (format "alignc %s " subcommand))))
 
 (defun align-run ()
   "Run the current align file."
   (interactive)
-  (let* ((proj-root (locate-dominating-file default-directory "Cargo.toml"))
-         (cargo-cmd (if proj-root 
-                        (format "cargo run -q --manifest-path %sCargo.toml --bin alignc -- run " proj-root)
-                      "cargo run -q --bin alignc -- run ")))
-    (compile (format "%s%s" cargo-cmd (shell-quote-argument buffer-file-name)))))
+  (compile (format "%s%s" (align--command "run")
+                   (shell-quote-argument buffer-file-name))))
 
 (defun align-check ()
   "Check (lint) the current align file."
   (interactive)
-  (let* ((proj-root (locate-dominating-file default-directory "Cargo.toml"))
-         (cargo-cmd (if proj-root 
-                        (format "cargo run -q --manifest-path %sCargo.toml --bin alignc -- check " proj-root)
-                      "cargo run -q --bin alignc -- check ")))
-    (compile (format "%s%s" cargo-cmd (shell-quote-argument buffer-file-name)))))
+  (compile (format "%s%s" (align--command "check")
+                   (shell-quote-argument buffer-file-name))))
 
 (defun align-fmt ()
   "Format the current align file."
   (interactive)
   (when (buffer-modified-p)
     (save-buffer))
-  (let* ((proj-root (locate-dominating-file default-directory "Cargo.toml"))
-         (cargo-cmd (if proj-root 
-                        (format "cargo run -q --manifest-path %sCargo.toml --bin alignc -- fmt " proj-root)
-                      "cargo run -q --bin alignc -- fmt ")))
-    (shell-command (format "%s%s --write" cargo-cmd (shell-quote-argument buffer-file-name)))
-    (revert-buffer t t t)))
+  (shell-command (format "%s%s --write" (align--command "fmt")
+                         (shell-quote-argument buffer-file-name)))
+  (revert-buffer t t t))
 
 ;;;###autoload
 (define-derived-mode align-mode prog-mode "Align"
--- editors/vim/syntax/align.vim
+++ editors/vim/syntax/align.vim
@@ -2,17 +2,27 @@ if exists("b:current_syntax")
   finish
 endif
 
-syn keyword alignKeyword fn return mut pub module import if else arena box task_group match template unsafe extern as out
-syn keyword alignType i8 u8 i16 u16 i32 u32 i64 u64 f32 f64 bool char str string array slice raw writer reader buffer rng tcp_conn tcp_listener udp_socket child soa Option Result vec2 vec4 vec8 vec16 mask2 mask4 mask8 mask16
-syn keyword alignBuiltin map par_map where reduce scan partition group_by sort sort_by_key chunks sum min max count any all dot
+syn keyword alignKeyword fn return mut pub module import if else arena task_group match loop break template unsafe extern as out
+syn match alignAttribute "\<\(align\|layout\|link\)\ze\s*("
+syn keyword alignType i8 u8 i16 u16 i32 u32 i64 u64 f32 f64 bool char str string array slice soa box raw builder writer reader buffer array_builder file rng tcp_conn tcp_listener udp_socket child Option Result Error Num Ord Eq vec2 vec4 vec8 vec16 mask2 mask4 mask8 mask16
+syn keyword alignBuiltin map par_map where reduce scan partition group_by agg sort sort_by_key chunks zip to_array map_into to_soa dict_encode sum min max count any all dot sum_where select fma abs sqrt floor ceil round trunc pow Some None Ok Err error map_err spawn wait print
 syn keyword alignBoolean true false
 
 syn match alignComment "//.*$"
-syn region alignString start=+"+ skip=+\\\\\|\\"+ end=+"+
-syn region alignChar start=+'+ skip=+\\'+ end=+'+
-syn match alignNumber "\<[0-9_]\+\(\.[0-9_]\+\)\?\([eE][+-]\?[0-9_]\+\)\?\>"
+syn region alignString start=+"+ skip=+\\\\\|\\"+ end=+"+ oneline
+syn region alignChar start=+'+ skip=+\\'+ end=+'+ oneline
+syn match alignNumber "\<[0-9][0-9_]*\>"
+syn match alignNumber "\<0[xX][0-9A-Fa-f_]\+\>"
+syn match alignNumber "\<0[oO][0-7_]\+\>"
+syn match alignNumber "\<0[bB][01_]\+\>"
+syn match alignFloat "\<[0-9][0-9_]*\.[0-9][0-9_]*\([eE][+-]\?[0-9][0-9_]*\)\?\>"
+syn match alignFloat "\<[0-9][0-9_]*[eE][+-]\?[0-9][0-9_]*\>"
+syn match alignOperator ":=\|->\|=>\|==\|!=\|<=\|>=\|&&\|<<\|>>\|\.\."
+syn match alignOperator "||"
+syn match alignOperator "[=+*/%<>!&|^~?-]"
 
 hi def link alignKeyword Keyword
+hi def link alignAttribute PreProc
 hi def link alignBuiltin Function
 hi def link alignType Type
 hi def link alignBoolean Boolean
@@ -20,5 +30,7 @@ hi def link alignComment Comment
 hi def link alignString String
 hi def link alignChar Character
 hi def link alignNumber Number
+hi def link alignFloat Float
+hi def link alignOperator Operator
 
 let b:current_syntax = "align"
